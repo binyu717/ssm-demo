c6608f7f105202ba72dace340c93f6825a55409c
bin.yu <yubin129@qq.com>
2018-08-18 17:01:22 +0800

    update

--- src/main/java/cn/yu/controller/ProductsController.java
+++ src/main/java/cn/yu/controller/ProductsController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("products")
 public class ProductsController {
 
+
     @Autowired(required=false)
     private ProductsService productsService;
 
